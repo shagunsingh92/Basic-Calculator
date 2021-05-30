79a62bf4abbcbeb386ad3c442327504796cbfe0f
shagunsingh92 <54054538+shagunsingh92@users.noreply.github.com>
2021-05-31 03:02:45 +0530

    Update AstrologerStarPattern.py

--- AstrologerStarPattern.py
+++ AstrologerStarPattern.py
@@ -2,41 +2,19 @@ print(''' Astrologer's star pattern
         - Created by Shagun Singh\n\n''')
 
 my_variable = '*'
-my_list=[] #this list is needed to reverse the pattern
+my_list=[] # needed for astrologer star
 
-while True:# this will decide the number of rows
-    n =int(input('Enter a number between 4 to 6:'))
-    if n<4:
-        print('You have entered a number smaller than 4\n')
-        continue
-    elif n>6:
-        print('You have entered a number greater than 6\n')
-        continue
-    else:
-        break
+my_stars = int(input("How many rows do you want in your astrologer's star: "))
 
-while True:
-    #This loop will ensure that the boolean number entered is either 0 or 1. 
-    #This was done because bool always returns true except when int(0) is passed to it
-    
-    my_num=int(input('\nEnter either 1 or 0: ')) #keep asking for 0 or 1 until given
-    if my_num==0 or my_num==1: 
-        break
-    else:
-        continue
-        
-my_bool=bool(my_num) #turns int into true or false
+for num in range(1,my_stars+1):
+    new_var= my_variable*num
+    my_list.append(new_var)
 
 print("\nHere is your Astrologer's star pattern:\n")
+for items in my_list:
+    print(items)
 
-if my_bool==True:
-    for num in range(1,n+1):
-        print(my_variable*num)# rows will get changed automatically as print takes an end operator whose default value is \n
-        
-else:
-    for num in range(1,n+1):
-        my_list.append(my_variable*num)# keep appending the * to empty list so that it can be reversed later
-        
-my_new=my_list[::-1]
-for num in range(0,len(my_new)):
-    print(my_new[num]) # printed the reversed list of *
+print("\nHere is your reverse Astrologer's star pattern:\n")             
+my_new= my_list[::-1] # needed for reverse astrologer star series
+for items in my_new:
+    print(items) # printed the reversed list of *
